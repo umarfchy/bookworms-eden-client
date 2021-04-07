5bbe494eebf455d675ff70f1e6a19abb1b593b9f
umar <unzippinglinux@gmail.com>
2021-04-08 01:34:52 +0600

    checkout added newly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import AdminArea from './components/AdminArea/AdminArea';
+import Checkout from './components/Checkout/Checkout';
 import Homepage from './components/Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
       {/* <Homepage></Homepage> */}
-      <AdminArea></AdminArea>
+      {/* <AdminArea></AdminArea> */}
     </div>
   );
 }
