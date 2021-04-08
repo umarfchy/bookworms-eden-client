dc92aac2c17b27346d7e49c64423d1281ccc3e73
umar <unzippinglinux@gmail.com>
2021-04-08 23:44:26 +0600

    firebase added

---
+++ src/components/LoginFirebase/LoginFirebase.js
@@ -0,0 +1,40 @@
+import React, { useContext} from 'react';
+import './LoginFirebase.css'
+import googleIcon from './../../Images/icons/google_logo.svg'
+// import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router';
+import { handleGoogleLogin, handleSignOut, initLoginFramework } from '../../loginManager';
+
+const LoginFirebase = () => {
+
+    // const [user, setUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    let { from } = location.state || { from: { pathname: "/" } };
+
+    initLoginFramework();
+    
+    const googleLogin = ()=>{
+        handleGoogleLogin()
+        .then(res => {
+            setUser(res)
+            history.replace(from)
+        })
+    }
+
+    return (
+        <div>
+            <div className='firebaseSignUp'>
+            <div onClick={googleLogin}> 
+            <div id='hrDiv'><hr id='leftHr' /> Or <hr id='rightHr' /></div>
+            <div className='googleSignUp'>
+                <img src={googleIcon} alt=""/>
+                <p>Continue with Google</p>
+            </div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default LoginFirebase;
