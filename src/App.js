833f24bfb9ae2aab92f1b7d9afed24efdc1549fb
umar <unzippinglinux@gmail.com>
2021-05-07 04:48:38 +0600

    react bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,47 @@
-import './App.css';
-import AdminArea from './components/AdminArea/AdminArea';
-import Checkout from './components/Checkout/Checkout';
-import Homepage from './components/Homepage/Homepage';
+import "./App.css";
+import AdminArea from "./components/AdminArea/AdminArea";
+import Checkout from "./components/Checkout/Checkout";
+import Homepage from "./components/Homepage/Homepage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import LoginFirebase from './components/LoginFirebase/LoginFirebase';
-import { createContext, useState} from 'react';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import LoginFirebase from "./components/LoginFirebase/LoginFirebase";
+import { createContext, useState } from "react";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const UserContext = createContext();
 
 function App() {
   const [user, setUser] = useState({
     isSignedIn: false,
-    name:'',
-    email:'',
-    password:''
-});
+    name: "",
+    email: "",
+    password: "",
+  });
 
   return (
     <div className="App">
-      <UserContext.Provider value= {[user, setUser]}>
-      <Router >
-        <Switch>
-          <Route exact path='/'>
-            <Homepage></Homepage>
-          </Route>
-          <Route path='/login'>
-            <LoginFirebase></LoginFirebase>
-          </Route>
-          <PrivateRoute path='/admin'>
-            <AdminArea></AdminArea>
-          </PrivateRoute>
-          <PrivateRoute path='/checkout'>
-            <Checkout></Checkout>
-          </PrivateRoute>
-          <Route path='/*'>
-          <div>
-            <h1>Nothing was found</h1>
-          </div>
-          </Route>
-        </Switch>
-      </Router>
+      <UserContext.Provider value={[user, setUser]}>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Homepage></Homepage>
+            </Route>
+            <Route path="/login">
+              <LoginFirebase></LoginFirebase>
+            </Route>
+            <PrivateRoute path="/admin">
+              <AdminArea></AdminArea>
+            </PrivateRoute>
+            <PrivateRoute path="/checkout">
+              <Checkout></Checkout>
+            </PrivateRoute>
+            <Route path="/*">
+              <div>
+                <h1>Nothing was found</h1>
+              </div>
+            </Route>
+          </Switch>
+        </Router>
       </UserContext.Provider>
     </div>
   );
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import './Cards.css'
+import React from "react";
+// import './Cards.css'
 
-const Cards = (props) => {
-    console.log(props);
-    const {imgUrl, bookName, authorName, price} = props.bookInfo;
-    return (
-        <div>
-            <div className='cardMain'>
-                <div className='cardDesc'>
-                    <img src={imgUrl} alt="" className="cardImg"/>
-                    <div className='cardDesc'>
-                    <h3>{bookName}</h3>
-                    <p>{authorName}</p>
-                    </div>
-                </div>
-                <div className='cardPrice'>
-                    <p>${price}</p>
-                    <button className='cardBuyNow'>Buy Now</button>
-                </div>
-            </div>
+const Cards = props => {
+  console.log(props);
+  const { imgUrl, bookName, authorName, price } = props.bookInfo;
+  return (
+    <div>
+      <div className="cardMain">
+        <div className="cardDesc">
+          <img src={imgUrl} alt="" className="cardImg" />
+          <div className="cardDesc">
+            <h3>{bookName}</h3>
+            <p>{authorName}</p>
+          </div>
         </div>
-    );
+        <div className="cardPrice">
+          <p>${price}</p>
+          <button className="cardBuyNow">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
